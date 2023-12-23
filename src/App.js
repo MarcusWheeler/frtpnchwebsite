dec4ab2136c58ef3b4bf1bcaa480ada9b59a53ae
MarcusWheeler <wheemarc@oregonstate.edu>
2023-12-23 14:51:43 -0800

    Placeholder website frtpnch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactPlayer from 'react-player';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const videoId = 'pn77HwlK0rI';
+
+  // Discord link using the server ID
+  const discordLink = `https://discord.gg/r58hbTREAS`;
+  const powerRankingsImage = process.env.PUBLIC_URL + 'pr.jpg';
+    return (
+    <div>
+      <h1>Hello, World!</h1>
+      <ReactPlayer
+        url={`https://www.youtube.com/watch?v=${videoId}`}
+        playing={true} // Set to true for autoplay
+        controls={true} // Show video controls
+        width="640px"
+        height="360px"
+        muted={true}  // Mute the video
+      />
+      <p>
+        Join our Discord community: <a href={discordLink} target="_blank" rel="noopener noreferrer">Click here</a>
+      </p>
+      <p>
+        Power Rankings:
+        {/* Display the image directly on the page */}
+        <img src={powerRankingsImage} alt="Power Rankings" style={{ maxWidth: '100%' }} />
+      </p>
     </div>
   );
 }
 
 export default App;
+
